d1bc57006642e28c549ed90b846c9e7646f69444
yildirim97-d <yildirim97d@gmail.com>
2021-10-17 13:05:17 +0300

    WinFormArkadasSayiBulma

---
+++ ArkadasSayiBulma/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace ArkadasSayiBulma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.XLabel = new System.Windows.Forms.Label();
+            this.YLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // XLabel
+            // 
+            this.XLabel.AutoSize = true;
+            this.XLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.XLabel.Location = new System.Drawing.Point(50, 35);
+            this.XLabel.Name = "XLabel";
+            this.XLabel.Size = new System.Drawing.Size(21, 20);
+            this.XLabel.TabIndex = 0;
+            this.XLabel.Text = "X";
+            // 
+            // YLabel
+            // 
+            this.YLabel.AutoSize = true;
+            this.YLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.YLabel.Location = new System.Drawing.Point(50, 100);
+            this.YLabel.Name = "YLabel";
+            this.YLabel.Size = new System.Drawing.Size(20, 20);
+            this.YLabel.TabIndex = 1;
+            this.YLabel.Text = "Y";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ARKADAŞ MI";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(185, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "SON";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 254);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.YLabel);
+            this.Controls.Add(this.XLabel);
+            this.Name = "Form1";
+            this.Text = "Arkadaş Sayılar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label XLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label YLabel;
+    }
+}
+
---
+++ ArkadasSayiBulma/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArkadasSayiBulma
+{
+    public partial class Form1 : Form
+    {
+        /* Global olarak tanımladığımız değişkenler aşağıdadır. Arkadaş mı butonuna tıkladıktan sonra dinamik olarak eklenmesi için
+           ve verileri bu eventlere yazdırmakta kullanacağımız yapıdır.
+        */
+        // dinamik olarak ekleyeceğimiz textbox değişkeni. Girilen sayının çarpanlarının toplamını gösterecektir.
+        public TextBox textbox;
+        // dinamik olarak ekleyeceğimiz listbox değişkeni. Girilen sayının çarplanlarını tek tek burada gösterilecektir.
+        public ListBox listbox;
+        // dinamik olarak ekleyeceğimiz label değişkeni. X,Y ve TOPLAMLAR olarak Text özelliğinden yararlanacağımız label.
+        public Label label;
+        // XsayisiCarpanToplam değişkenine girilen X sayısının çarpanlarının toplamı atanacaktır.
+        int XsayisiCarpanToplam = 0;
+        // YsayisiCarpanToplam değişkenine girilen Y sayısının çarpanlarının toplamı atanacaktır.
+        int YsayisiCarpanToplam = 0;
+        // Sayıların arkadaş mı olup olmadıklarını yazdıracağımız string bir mesaj;
+        string mesaj;
+        // Arkadaş mı butonuna bir kere basmamızı sağlar böylelikle eventler ve olaylar tekrarlanmaz.
+        int sayac = 0;
+        // textbox1de ki girilecek olan sayıya atanan değer ( X sayısı )
+        int GirilenXSayisi;
+        // textbox2de ki girilecek olan sayıya atanan değer ( Y sayısı )
+        int GirilenYSayisi;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // ARKADAŞ MI BUTONU
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // sayac 0 iken buton bir kere çalışacak
+            if(sayac == 0)
+            {
+                // herhangi bir karakter girilmez ise veya 0 girilirse boş karakter girilmez mesajı gösterecektir ve tekrar dönecektir.
+                if ((textBox1.Text == "" || textBox2.Text == "") || (textBox1.Text == "0" || textBox2.Text == "0"))
+                {
+                    
+                    MessageBox.Show("BOŞ KARAKTER GİRİLEMEZ! veya  0 KARAKTERİ KULLANILAMAZ!");
+                    return;
+                }
+                // girilen X sayısını int tipine dönüştürüyoruz
+                GirilenXSayisi = Convert.ToInt32(textBox1.Text);
+                // girilen Y sayısını int tipine dönüştürüyoruz
+                GirilenYSayisi = Convert.ToInt32(textBox2.Text);
+               
+
+                #region Dinamik Events
+                // butona tıklandıktan sonra formun boyutunu tekrar belirliyoruz.
+                Size = new Size(750, 420);
+
+
+                // burada for 2 kere dönüldü çünkü label 3 textbox 2  listbox 2 kere oluşturulacağından, textbox'un eksik kalanını aşağıda belirledik böylelikle daha kolay oldu
+            for (int i = 0; i < 2; i++)
+            {
+                    // her döngüde tekrar newleyerek yeni bir textbox oluşturuyor.
+                textbox = new TextBox();
+                    // her döngüde tekrar newleyerek yeni bir listbox oluşturuyor.
+                    listbox = new ListBox();
+                    // her döngüde tekrar newleyerek yeni bir label oluşturuyor.
+                    label = new Label();
+                    // label 2 TOPLAMLAR texti için kullanılacaktır.
+                    label2 = new Label();
+
+                    // birinci döngüdeki olayları yakalamak için böyle bir if yapısı oluşturulmak zorunda kalındı.
+                    // genelde 2 adet dinamik event oluşturulacağı için birinci döngüde ve (else) 2. döngüde yapılacaklar belirlendi.
+                if (i == 0)
+                {
+                    //label text özelliğine X atandı
+                    label.Text = "X";
+                    //label ın pozisyonu set edildi
+                    label.Location = new Point(400, 20);
+                    //listbox ın boyutları set edildi
+                    listbox.Size = new Size(125, 250);
+                    //listbox ın pozisyonu set edildi
+                    listbox.Location = new Point(350, 60);
+                    //textbox ın boyutları set edildi
+                    textbox.Size = new Size(125, 50);
+                    //textbox ın pozisyonu set edildi
+                    textbox.Location = new Point(350, 320);
+                    // label2 nin boyutu set edildi
+                    label2.Size = new Size(100, 100);
+                    // label2 nin texti set edildi
+                    label2.Text = "TOPLAMLAR ";
+                    // label2 nin pozisyonu set edildi
+                    label2.Location = new Point(275, 323);
+
+                        
+                        //Burada girilen X sayısının çarpanlarını ayırma işlemini yapıyoruz.
+                        //j yi 1 den başlatmak şartıyla girilen sayıdan küçük oluncaya kadar..
+                        for (int j = 1; j < GirilenXSayisi; j++)
+                    {
+                            //ve eğer X sayısı 1 den başlayıp X sayısına kadar olan sayıların ( teker teker )
+                            if (GirilenXSayisi % j == 0)
+                        {
+
+                           
+                                // modu yani 0 a kalansız bölünüyorsa ( çarpanlarıdır bunlar ) bu sayıları listboxa ekle.
+                                listbox.Items.Add(j);
+                                //Her çarpanı XsayisiCarpanToplam a toplayarak set et.
+                               XsayisiCarpanToplam += j;
+                                // ve son olarak textbox değerine XsayisiCarpanToplam değerini set et.
+                                textbox.Text = XsayisiCarpanToplam.ToString();
+                        }
+                    }
+                }
+
+                else
+                {
+                    // label2 ye Y set edildi.
+                    label.Text = "Y";
+                    //label ın pozisyonu set edildi
+                    label.Location = new Point(600, 20);
+                    //listbox ın boyutu set edildi
+                    listbox.Size = new Size(125, 250);
+                    //listbox ın pozisyonu set edildi
+                    listbox.Location = new Point(550, 60);
+                    //textbox ın boyutları set edildi.
+                    textbox.Size = new Size(125, 50);
+                    //textbox ın pozisyonu set edildi.
+                    textbox.Location = new Point(550, 320);
+
+
+                        //Girilen Y sayısının çarpanlarını ayırma işlemini yapıyoruz.
+                        //j yi 1 den başlatmak şartıyla girilen sayıdan küçük oluncaya kadar..
+                        for (int j = 1; j < GirilenYSayisi; j++)
+                    {
+                            //ve eğer X sayısı 1 den başlayıp Y sayısına kadar olan sayıların ( teker teker )
+                            if (GirilenYSayisi % j == 0)
+                        {
+                          
+                            // modu yani 0 a kalansız bölünüyorsa ( çarpanlarıdır bunlar ) bu sayıları 2.listboxa ekle.
+                            listbox.Items.Add(j);
+                            //Her çarpanı YsayisiCarpanToplam a toplayarak set et.
+                            YsayisiCarpanToplam += j;
+                            // ve son olarak textbox değerine YsayisiCarpanToplam değerini set et.
+                            textbox.Text = YsayisiCarpanToplam.ToString();
+                        }
+
+                    }
+
+                }
+                // girilen X ve Y ortak oldukları için (boyutları) sorgu dışında set ediyoruz.
+                label.Size = new Size(35, 35);
+
+                // formumuza dinamik olarak label' i ekliyoruz.   
+                Controls.Add(label);
+                // formumuza dinamik olarak listbox' ı ekliyoruz.
+                Controls.Add(listbox);
+                // formumuza dinamik olarak textbox' ı ekliyoruz.
+                Controls.Add(textbox);
+                // formumuza dinamik olarak label2' yi ekliyoruz.
+                Controls.Add(label2);
+            }
+            
+            #endregion
+            // girilen x sayısı ile Girilen Y sayısının çarplanları toplamı eşit ise ve girilen y sayısı ile Girilen X sayısının çarpanları toplamı eşit ise
+            if (GirilenXSayisi == YsayisiCarpanToplam && GirilenYSayisi == XsayisiCarpanToplam)
+            {
+                    // mesaja bu 2 sayı arkadaştır ı set ediyoruz
+                mesaj = "Bu 2 Sayı ARKADAŞ !";
+            }
+            // değilse
+            else
+                    // bu 2 sayı arkadaş değişdiri set ediyoruz ve..
+                mesaj = "Bu 2 Sayı ARKADAŞ DEĞİL !";
+            // kullanıcıya burada mesajı gösteriyoruz.
+            MessageBox.Show(mesaj);
+            }
+            // sayacı bir kere attırarak butona bir daha basma iznini ortadan kaldırıyoruz.
+            sayac++;
+        }
+
+
+
+        // BOŞ.
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        // textbox1 e sadece rakam girilmesine izin verildi
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        // textbox2 ye sadece rakam girilmesine izin verildi.
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        // Son butonu ile uygulamadan çıkılıyor.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
